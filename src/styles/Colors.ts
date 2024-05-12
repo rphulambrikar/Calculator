a7fd47669bb877ad3ec2269645e3678ce1b51e0f
rphulambrikar <rohitphulambrikar03@gmail.com>
2024-05-12 21:34:30 +0530

    Initial Commit

---
+++ src/styles/Colors.ts
@@ -0,0 +1,11 @@
+export const myColors = {
+  light: '#F1F2F3',
+  dark: '#17171C',
+  blue: '#f5b31b',
+  btnGray: '#c9ccd1',
+  btnDark: '#2E2F38',
+  gray: '#373738',
+  black: '#000000',
+  white: '#FFFFFF',
+  result: '#46D5B2',
+};
